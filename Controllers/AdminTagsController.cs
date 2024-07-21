9a220157dd3811a83f566d5bf3ae3dda23f26402
Yeseul Kim <yk170901@gmail.com>
2024-07-21 21:35:35 +0900

    Modify Layout.cshtml & Add Controllers and Views for Tag Creation Functionality

---
+++ Controllers/AdminTagsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        // AdminTags/Add
+        [HttpGet]
+        public IActionResult Add()
+        {
+            // view for adding a tag
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 using Project.Data;
-using Pomelo.EntityFrameworkCore.MySql;
 using Microsoft.EntityFrameworkCore;
 
 
@@ -8,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// will it work?
+// for mysql - pomelo needed
 builder.Services.AddDbContext<BlogDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("BlogDbConnectionString"), new MySqlServerVersion(new Version(8,0,38)))
 );
