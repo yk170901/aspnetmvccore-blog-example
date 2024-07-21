3ca79a3577be27068afc920ccd1323cd268f52d7
Yeseul Kim <yk170901@gmail.com>
2024-07-21 22:00:36 +0900

    Get user inputs from a page

--- Controllers/AdminTagsController.cs
+++ Controllers/AdminTagsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Project.Models.ViewModels;
 
 namespace Project.Controllers
 {
@@ -10,6 +11,25 @@ namespace Project.Controllers
         {
             // view for adding a tag
             return View();
+        }
+
+        [HttpPost]
+        // [ActionName("Add")] // the name is Add.cshtml; thus, Add. can use when the method name is dif from the action name
+        public IActionResult Add(AddTagRequest reqValue)
+        {
+            #region Method 1 : Manual Reading Inputs
+            // Request.Form[value of the property "name" of the input element]
+            // string name = Request.Form["name"].ToString();
+            // string displayName = Request.Form["displayName"].ToString();
+            #endregion
+
+            #region Method 2(Better) : Model Binding
+            
+            string name = reqValue.Name;
+            string displayName = reqValue.DisplayName;
+            #endregion
+
+            return View("Add");
         }
     }
 }
---
+++ Models/ViewModels/AddTagRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Project.Models.ViewModels
+{
+    public class AddTagRequest
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
