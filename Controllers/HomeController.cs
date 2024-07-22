7263cfab7a27ce818b0bd17c360d27a9af613152
yk170901 <yk170901@gmail.com>
2024-07-22 20:46:03 +0900

    show all tags in index page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Project.Models;
 using Project.Models.Domain;
+using Project.Models.ViewModels;
 using Project.Repositories;
 using System.Diagnostics;
 
@@ -10,18 +11,25 @@ namespace Project.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IBlogPostRepository _blogPostRepository;
+        private readonly ITagRepository _tagRepository;
 
-        public HomeController(ILogger<HomeController> logger, IBlogPostRepository blogPostRepository)
+        public HomeController(ILogger<HomeController> logger, IBlogPostRepository blogPostRepository, ITagRepository tagRepository)
         {
             _logger = logger;
             _blogPostRepository = blogPostRepository;
+            _tagRepository = tagRepository;
         }
 
         public async Task<IActionResult> Index()
         {
             IEnumerable<BlogPost> blogPosts = await _blogPostRepository.GetAllAsync();
+            IEnumerable<Tag> tags = await _tagRepository.GetAllAsync();
 
-            return View(blogPosts);
+            return View(new HomeViewModel()
+            {
+                BlogPosts = blogPosts,
+                Tags = tags
+            });
         }
 
         public IActionResult Privacy()
---
+++ Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Project.Models.Domain;
+
+namespace Project.Models.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<BlogPost> BlogPosts { get; set; }
+        public IEnumerable<Tag> Tags { get; set; }
+    }
+}
