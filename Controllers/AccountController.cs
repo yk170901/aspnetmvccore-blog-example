4e842d9aed659149fbd177ec040e1d9c144e263b
yk170901 <yk170901@gmail.com>
2024-07-23 10:41:23 +0900

    Login (1/2) - view login page, fill info, get confirmed

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -7,11 +7,15 @@ namespace Project.Controllers
     public class AccountController : Controller
     {
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
 
-        // DOTNET FEATURE = user manager service - create, register users to db
-        public AccountController(UserManager<IdentityUser> userManager)
+        // DOTNET FEATURE
+        // 1. user manager service - create, register users to db
+        // 2. sign in user
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         [HttpGet]
@@ -44,6 +48,27 @@ namespace Project.Controllers
             }
 
             return View("Register");
+        }
+
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginInfo)
+        {
+            Microsoft.AspNetCore.Identity.SignInResult? signInResult = await _signInManager.PasswordSignInAsync(loginInfo.UserName, loginInfo.Password, false, false);
+
+            if (signInResult != null
+                && signInResult.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
         }
     }
 }
